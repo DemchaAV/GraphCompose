0f874a297fb6b2e7bd444a8c94ee702dbaa8fd2d
DemchaAV <demchaav@gmail.com>
2025-11-18 13:55:32 +0000

    Refactoring encapsulation  guideRender in to PdfGuideLineRender

--- src/main/java/com/demcha/system/RenderingSystemECS.java
+++ src/main/java/com/demcha/system/RenderingSystemECS.java
@@ -5,6 +5,6 @@ import com.demcha.components.components_builders.Canvas;
 public interface RenderingSystemECS extends SystemECS {
     <T extends Canvas> T canvas();
     <T extends GuidesRenderer> T guideRenderer();
-    <T extends RenderStream> T stream();
+    <T,S extends RenderStream<S>> S stream();
 
 }

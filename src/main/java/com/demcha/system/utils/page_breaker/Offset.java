1b0ca0b5f0f8ad7603f65f591f3e3d8c3a566b71
DemchaAV <demchaav@gmail.com>
2025-12-02 09:36:38 +0000

    Fixed backbreakingContainer finalization beta version

--- src/main/java/com/demcha/system/utils/page_breaker/Offset.java
+++ src/main/java/com/demcha/system/utils/page_breaker/Offset.java
@@ -6,49 +6,53 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.atomic.DoubleAdder;
+
 @Slf4j
 @Data
 @Accessors(fluent = true)
-@AllArgsConstructor
 @NoArgsConstructor
 public class Offset {
-    private double y = 0.0;
-    private double x = 0.0;
+    // Use DoubleAdder for thread safety and performance
+    private final DoubleAdder y = new DoubleAdder();
+    private final DoubleAdder x = new DoubleAdder();
 
-    public double incrementY(float increment) {
-        return incrementY(Double.valueOf(increment));
+    // Custom getters because DoubleAdder doesn't return primitive double automatically
+    public double y() {
+        return y.sum();
     }
 
-    public double incrementY(int increment) {
-        return incrementY(Double.valueOf(increment));
+    public double x() {
+        return x.sum();
     }
 
-    public double incrementY(double increment) {
+    // --- Y Increments ---
+    public void incrementY(double increment) {
         log.debug("{} increment Y({})", this, increment);
-        return this.y += increment;
+        this.y.add(increment);
     }
 
-    public double incrementY(Offset offset) {
+    // Overloads
+    public void incrementY(float increment) { incrementY((double) increment); }
+    public void incrementY(int increment) { incrementY((double) increment); }
+
+    public void incrementY(Offset offset) {
         log.debug("Increment offset Y from object {}", offset);
-        return incrementY(offset.y);
+        incrementY(offset.y());
     }
 
-    public double incrementX(float increment) {
-        return incrementY(Double.valueOf(increment));
-    }
-
-    public double incrementX(int increment) {
-        return incrementY(Double.valueOf(increment));
-    }
-
-    public double incrementX(double increment) {
+    // --- X Increments (FIXED BUGS HERE) ---
+    public void incrementX(double increment) {
         log.debug("{} increment X ({})", this, increment);
-        return this.x += increment;
+        this.x.add(increment); // Was adding to Y in your code
     }
 
-    public double incrementX(Offset offset) {
+    // Overloads
+    public void incrementX(float increment) { incrementX((double) increment); } // Fixed call
+    public void incrementX(int increment) { incrementX((double) increment); }   // Fixed call
+
+    public void incrementX(Offset offset) {
         log.debug("Increment offset X from object {}", offset);
-        return incrementY(offset.x);
+        incrementX(offset.x()); // Fixed call
     }
-
 }
